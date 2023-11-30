091e9bd97cc56594724af38048e45cce9cde5dae
Ihor Chernomorets <oxnardxvi@gmail.com>
2023-12-01 00:09:45 +0100

    Added NULL_NAME_ERROR constant to the ProfileProperties class for fixing quality duplication issue.

--- src/main/java/com/epam/mjc/io/ProfileProperties.java
+++ src/main/java/com/epam/mjc/io/ProfileProperties.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 public class ProfileProperties {
 
+    private static final String NULL_NAME_ERROR = "Name cannot be null.";
+
     private final Map<String, String> properties;
 
     public ProfileProperties() {
@@ -13,7 +15,7 @@ public class ProfileProperties {
 
     public void put(String name, String value) {
         if (name == null) {
-            throw new IllegalArgumentException("Name cannot be null.");
+            throw new IllegalArgumentException(NULL_NAME_ERROR);
         }
         if (value == null) {
             throw new IllegalArgumentException("Value cannot be null.");
@@ -24,14 +26,14 @@ public class ProfileProperties {
 
     public String getString(String name) {
         if (name == null) {
-            throw new IllegalArgumentException("Name cannot be null.");
+            throw new IllegalArgumentException(NULL_NAME_ERROR);
         }
         return properties.get(name);
     }
 
     public Integer getInteger(String name) {
         if (name == null) {
-            throw new IllegalArgumentException("Name cannot be null.");
+            throw new IllegalArgumentException(NULL_NAME_ERROR);
         }
         String value = properties.get(name);
 
@@ -40,11 +42,10 @@ public class ProfileProperties {
 
     public Long getLong(String name) {
         if (name == null) {
-            throw new IllegalArgumentException("Name cannot be null.");
+            throw new IllegalArgumentException(NULL_NAME_ERROR);
         }
         String value = properties.get(name);
 
         return value != null ? Long.parseLong(value) : null;
     }
-
 }
